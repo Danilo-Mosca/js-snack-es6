75da5591e2ff299c2c79b149b16caa554854f67c
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-12 16:30:25 +0100

    add random number in the object for Snack2

--- script/script.js
+++ script/script.js
@@ -45,10 +45,65 @@ console.log(biciLeggera);   // Visualizzo l'oggetto contenente la bici più legg
 const divBikes = document.querySelector("main #view");
 // Creo un template html contenente i dati della bici più leggera
 const template = `
-  <div className="card">
-  <h4>La bici più leggera e la: ${biciLeggera.nome}</h4>
-  <div>Il peso della bici è: ${biciLeggera.peso} kg</div>
+  <div>
+    <h4>La bici più leggera e la: ${biciLeggera.nome}</h4>
+    <div>Il peso della bici è: ${biciLeggera.peso} kg</div>
   </div>
   `;
 // Aggiungo il template alla pagina
 divBikes.innerHTML += template;
+
+
+
+/* Snack2
+Creare un array di oggetti di squadre di calcio.
+ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+const footballTeam = [
+    {
+        nome: "Milan",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Roma",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Lazio",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Fiorentina",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Udinese",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Atalanta",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Napoli",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+];
+
+for(let key in footballTeam) {
+    footballTeam[key].puntiFatti = getRndInteger(100, 1);
+    // console.log(footballTeam[key].puntiFatti = getRndInteger(100, 1));
+    footballTeam[key].falliSubiti = getRndInteger(100, 1);
+    // console.log(footballTeam[key].falliSubiti = getRndInteger(100, 1));
+}
+// console.log(footballTeam);
